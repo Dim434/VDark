e9e352d08f1ee3d75b5dc2d10bccfe85546e32fb
Dmitry <potdi@icloud.com>
2020-08-15 20:03:10 +0300

    Initial Commit

---
+++ VKApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKApp
+//
+//  Created by Dmitry on 15.08.2020.
+//  Copyright © 2020 g4play. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
